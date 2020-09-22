ef7f4e6381f02ceef442db32649dab8b930468b0
Daniel Turkington <turkington.ad@gmail.com>
2020-09-22 17:40:08 +0100

    Scraper targetting correct data, needs to be refined to extract correct details

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,12 @@
 class TeamsController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
+
   def index
-    @teams = Team.all
+    url = "https://en.wikipedia.org/wiki/List_of_English_football_champions"
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+
+    @teams = html_doc.search(".wikitable", ".a", "title")
   end
 end
